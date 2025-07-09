c83c532018d13acd09e613c352e0bbd085c88825
Coolp-GitHub <75265429+Coolp-GitHub@users.noreply.github.com>
2025-07-08 21:28:50 -0400

    all pieces have basic functionality. ADD EN PASSANT, PROMOTION, CASTLING.

--- src/main.cpp
+++ src/main.cpp
@@ -1,52 +1,54 @@
-#define SUPPORT_LONGLONG
 #include <Arduino.h>
 #include <FastLED.h>
 #include <limits.h>
 
 #include "moveGenerator.hpp"
 
-#define P 1
-#define B 2
-#define N 3
-#define R 4
-#define Q 5
-#define K 6
-#define p -1
-#define b -2
-#define n -3
-#define r -4
-#define q -5
-#define k -6
+constexpr int8_t P = 1;
+constexpr int8_t B = 2;
+constexpr int8_t N = 3;
+constexpr int8_t R = 4;
+constexpr int8_t Q = 5;
+constexpr int8_t K = 6;
 
-int8_t board1D[64] = {
+constexpr int8_t p = -1;
+constexpr int8_t b = -2;
+constexpr int8_t n = -3;
+constexpr int8_t r = -4;
+constexpr int8_t q = -5;
+constexpr int8_t k = -6;
+
+constexpr uint8_t WHITE_KING_START_POS = 60;  // e1
+constexpr uint8_t BLACK_KING_START_POS = 4;   // e8
+
+
+
+int8_t board1D[BOARD_SIZE] = {
     r, n, b, q, k, b, n, r,   // 8th rank (top)
     p, p, p, p, p, p, p, p,   // 7th rank
     0, 0, 0, 0, 0, 0, 0, 0,   // 6th rank
     0, 0, 0, 0, 0, 0, 0, 0,   // 5th rank
     0, 0, 0, 0, 0, 0, 0, 0,   // 4th rank
-    0, n, n, 0, 0, 0, 0, 0,   // 3rd rank
+    0, 0, 0, 0, 0, 0, 0, 0,   // 3rd rank
     P, P, P, P, P, P, P, P,   // 2nd rank
     R, N, B, Q, K, B, N, R    // 1st rank (bottom)
 };
 
 int64_t moves = 0LL;
-uint8_t whiteKingPosition = 60;
-uint8_t blackKingPosition = 4;
-
+uint8_t whiteKingPosition = WHITE_KING_START_POS;
+uint8_t blackKingPosition = BLACK_KING_START_POS;
+void printLongLong(int64_t move);
 void setup() 
 {
   Serial.begin(9600);
 
-  moves = generatePawnMoves(board1D,49);
+  int8_t row = 4;
+  int8_t col = 1;
+  int8_t testpos = row*8 + col;
+  board1D[testpos] = n;
+  moves = generateKnightMoves(board1D, testpos);
   Serial.print("\n");
-  for(int i = 0; i <=63; i++)
-  {
-    if (i%8 == 0)
-    {
-      Serial.print("\n");
-    }
-    Serial.print(int((moves >> i) & 1LL));
-  }
+  printLongLong(moves);
 
   // Serial.println("\n");
   // moves = generateKingMoves(board1D,whiteKingPosition);
@@ -57,23 +59,14 @@ void loop()
 {
 
 }
-/*
-00010100
-00011100
-00000000
-00000000
-00000000
-00000000
-00000000
-00000000
 
-
-00000000
-00000000
-00000000
-00000000
-00000000
-00000000
-00000011
-10000010
-*/
+void printLongLong(int64_t move){
+for(int i = 0; i <=63; i++)
+  {
+    if (i%8 == 0)
+    {
+      Serial.print("\n");
+    }
+    Serial.print(int((moves >> i) & 1LL));
+  }
+}
--- src/moveGenerator.hpp
+++ src/moveGenerator.hpp
@@ -2,103 +2,227 @@
 #define MOVE_GEN
 #include <Arduino.h>
 
-int64_t generateKingMoves(const int8_t board[64], int8_t position)
+
+constexpr int8_t BOARD_ROWS = 8;
+constexpr int8_t BOARD_COLS = 8;
+constexpr int8_t BOARD_SIZE = BOARD_ROWS * BOARD_COLS;
+
+
+constexpr int8_t BOARD_MIN_ROW = 0;
+constexpr int8_t BOARD_MIN_COL = 0;
+constexpr int8_t BOARD_MAX_ROW_INDEX = BOARD_ROWS - 1;
+constexpr int8_t BOARD_MAX_COL_INDEX = BOARD_COLS - 1;
+
+
+constexpr int8_t PAWN_WHITE_START_ROW = 6; 
+constexpr int8_t PAWN_BLACK_START_ROW = 1;  
+
+constexpr int8_t EMPTY = 0;
+
+
+int64_t generateKingMoves(const int8_t board[BOARD_SIZE], int8_t position)
 {
-   
+    int8_t baseRow = position / BOARD_COLS;
+    int8_t baseCol = position % BOARD_COLS;
     bool colour = board[position]>0; // true (1) = white, false (0) = black 
     
-    static const int8_t directions[8][2] = 
+    static constexpr int8_t directions[8][2] = 
     {
-        {  1,  0 }, 
-        {  1,  1 }, 
-        {  0,  1 }, 
-        { -1,  1 }, 
-        { -1,  0 }, 
-        { -1, -1 }, 
-        {  0, -1 },
-        {  1, -1 }  
-    };
+        {  1,  0 }, {  1,  1 }, {  0,  1 }, { -1,  1 }, 
+        { -1,  0 }, { -1, -1 }, {  0, -1 }, {  1, -1 }  
+    }; 
     int64_t moves = 0LL;
 
-    for(auto* pos: directions)
+    for(auto& pos: directions)
     {
-        int8_t row = position / 8;
-        int8_t col = position % 8;
         
-        row += pos[0];
-        col += pos[1];
+        int8_t row = baseRow + pos[0];
+        int8_t col = baseCol + pos[1];
 
-        int8_t pos1D = row*8 + col;
-
-        Serial.print(row);
-        Serial.print(" ");
-        Serial.print(col);
-        Serial.print(" ");
-        Serial.print(pos1D);
-        Serial.print("\n");
+        int8_t pos1D = row*BOARD_COLS + col;
         
         // 2 checks here
         // 1. within bounds of the board
         // 2. checks if the space to move to is either empty OR the enemy colour
-        if (row >= 0 && row <= 7 && col >= 0 && col <= 7 && (board[pos1D] == 0 || (board[pos1D] > 0) != colour))
+        if (row >= BOARD_MIN_ROW && row <= BOARD_MAX_ROW_INDEX&& col >= BOARD_MIN_COL && col <= BOARD_MAX_COL_INDEX)
         {            
+            if((board[pos1D] == EMPTY || (board[pos1D] > 0) != colour)){
             //bitwise or to update the move int64
             moves |= 1LL << pos1D;
+            }
         }
     }
     
     return moves;
 }
-
-int64_t generatePawnMoves(const int8_t board[64], int8_t position)
+//TAKES AN ATTACK ONLY BOOL DUE TO PAWNS NOT ATTACKING WHERE THEY MOVE
+int64_t generatePawnMoves(const int8_t board[BOARD_SIZE], int8_t position, bool attackingOnly) 
 {
-    int8_t row = position / 8;
-    int8_t col = position % 8;
+    int8_t baseRow = position / BOARD_COLS;
+    int8_t baseCol = position % BOARD_COLS;
     bool colour = board[position]>0; // true (1) = white, false (0) = black 
 
     int8_t forward = colour ? -1 : 1;
-    int8_t startRow = colour ? 6 : 1;
+    int8_t startRow = colour ? PAWN_WHITE_START_ROW : PAWN_BLACK_START_ROW;
 
     int64_t moves = 0LL;
-
-    int8_t nextPos1D = (row+forward)*8 + col;
-    if (row >= 0 && row <= 7 && col >= 0 && col <= 7)
+    int8_t updatedRow = baseRow+forward;
+    if (!attackingOnly) 
     {
-
-        if(board[nextPos1D] == 0)
+        if (updatedRow >= BOARD_MIN_ROW && updatedRow <= BOARD_MAX_ROW_INDEX)
         {
-            moves |= 1LL << nextPos1D;
-            if (row == startRow)
+            int8_t nextPos1D = (baseRow+forward)*BOARD_COLS + baseCol;
+
+            if(board[nextPos1D] == EMPTY)
             {
-                int8_t nextPos1D = (row+(forward*2))*8 + col;
-                if (row >= 0 && row <= 7 && col >= 0 && col <= 7 && board[nextPos1D] == 0)
+                moves |= 1LL << nextPos1D;
+                if (baseRow == startRow)
                 {
-                    moves |= 1LL << nextPos1D;
+                    int8_t doubleMovePos1D = (baseRow+(forward*2))*BOARD_COLS + baseCol;
+                    updatedRow = doubleMovePos1D / BOARD_COLS;
+                    if (updatedRow >= BOARD_MIN_ROW && updatedRow <= BOARD_MAX_ROW_INDEX)
+                    {
+                        if(board[doubleMovePos1D] == EMPTY){
+                            moves |= 1LL << doubleMovePos1D;
+                        }
+                    }
                 }
             }
         }
     }
     
-    // check attacks
-    nextPos1D = (row+forward)*8 + (col+1);
-    if (row >= 0 && row <= 7 && col >= 0 && col <= 7 &&  (board[nextPos1D] > 0) != colour && board[nextPos1D] != 0)
-    {
-        moves |= 1LL << nextPos1D;
-    }
-    nextPos1D = (row+forward)*8 + (col-1);
-    if (row >= 0 && row <= 7 && col >= 0 && col <= 7 && (board[nextPos1D] > 0) != colour && board[nextPos1D] != 0)
-    {
-        moves |= 1LL << nextPos1D;
+    
+    constexpr int8_t dc[2] = {1,-1};
+    for(int8_t change : dc){
+        updatedRow = baseRow+forward;
+        int8_t updatedCol = baseCol+change;
+        int8_t nextPos1D = updatedRow*BOARD_COLS + updatedCol;
+
+        if(updatedRow >= BOARD_MIN_ROW && updatedRow <= BOARD_MAX_ROW_INDEX && updatedCol >= BOARD_MIN_COL && updatedCol <= BOARD_MAX_COL_INDEX){
+            if ((board[nextPos1D] > 0) != colour && board[nextPos1D] != EMPTY)
+            {
+                moves |= 1LL << nextPos1D;
+            }
+        }
     }
     return moves;
 }
 
+int64_t generateRookMoves(const int8_t board[BOARD_SIZE], int8_t position)
+{
+    int8_t row = position / BOARD_COLS;
+    int8_t col = position % BOARD_COLS;
+    bool colour = board[position]>0; // true (1) = white, false (0) = black 
+    int64_t moves = 0LL;
+    auto scan = [&](int8_t dRow, int8_t dCol){
+        int8_t r = row + dRow;
+        int8_t c = col + dCol;
 
-
-
-
-int64_t generateEnemyAttacks(const int8_t board[64], bool colour){
-    
+        while (r >= BOARD_MIN_ROW && r <= BOARD_MAX_ROW_INDEX 
+            && c >= BOARD_MIN_COL && c <= BOARD_MAX_COL_INDEX)
+        {
+            int8_t pos1D = r*BOARD_COLS + c;
+            if(board[pos1D] == EMPTY)
+            {
+                moves |= 1LL << pos1D;
+            }else{
+                if ((board[pos1D] > 0) != colour){
+                    moves |= 1LL << pos1D;
+                }
+                break;
+            }
+            r+= dRow;
+            c+=dCol;
+        }
+    };
+    scan(1,0);
+    scan(0,1);
+    scan(-1,0);
+    scan(0,-1);
+    return moves;
 }
+
+int64_t generateBishopMoves(const int8_t board[BOARD_SIZE], int8_t position)
+{
+    int8_t row = position / BOARD_COLS;
+    int8_t col = position % BOARD_COLS;
+    bool colour = board[position]>0; // true (1) = white, false (0) = black 
+    int64_t moves = 0LL;
+    auto scan = [&](int8_t dRow, int8_t dCol){
+        int8_t r = row + dRow;
+        int8_t c = col + dCol;
+
+        while (r >= BOARD_MIN_ROW && r <= BOARD_MAX_ROW_INDEX 
+            && c >= BOARD_MIN_COL && c <= BOARD_MAX_COL_INDEX)
+        {
+            int8_t pos1D = r*BOARD_COLS + c;
+            if(board[pos1D] == EMPTY)
+            {
+                moves |= 1LL << pos1D;
+            }else{
+                if ((board[pos1D] > 0) != colour){
+                    moves |= 1LL << pos1D;
+                }
+                break;
+            }
+            r+= dRow;
+            c+= dCol;
+        }
+    };
+    scan(1,1);
+    scan(-1,1);
+    scan(1,-1);
+    scan(-1,-1);
+    return moves;
+}
+
+int64_t generateQueenMoves(const int8_t board[BOARD_SIZE], int8_t position){
+    int64_t moves = 0LL;
+    moves |= generateBishopMoves(board,position);
+    moves |= generateRookMoves(board,position);
+    return moves;
+}
+
+int64_t generateKnightMoves(const int8_t board[BOARD_SIZE], int8_t position)
+{
+    int8_t baseRow = position / BOARD_COLS;
+    int8_t baseCol = position % BOARD_COLS;
+    bool colour = board[position]>0; // true (1) = white, false (0) = black 
+    
+    static constexpr int8_t directions[8][2] =
+    {
+        {  2,  1 }, {  1,  2 },
+        { -1,  2 }, { -2,  1 },
+        { -2, -1 }, { -1, -2 },
+        {  1, -2 }, {  2, -1 }
+    };
+
+    int64_t moves = 0LL;
+
+    for(auto& pos: directions)
+    {
+        
+        int8_t row = baseRow + pos[0];
+        int8_t col = baseCol + pos[1];
+
+        int8_t pos1D = row*BOARD_COLS + col;
+
+        if (row >= BOARD_MIN_ROW && row <= BOARD_MAX_ROW_INDEX&& col >= BOARD_MIN_COL && col <= BOARD_MAX_COL_INDEX)
+        {            
+            if((board[pos1D] == EMPTY || (board[pos1D] > 0) != colour)){
+            moves |= 1LL << pos1D;
+            }
+        }
+    }
+    
+    return moves;
+}
+
+
+int64_t generateEnemyAttacks(const int8_t board[BOARD_SIZE], bool colour){
+    // TODO: IMPLENET THIS AFTER ALL OTHER MOVES FOR CHECK DETECTION
+    return 0LL;
+}
+
 
 #endif
